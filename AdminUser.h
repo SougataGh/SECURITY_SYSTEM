8f4784c73d61dfd4fc8f5a2ed28454a116d8bfec
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-07 09:39:07 +0530

    Update AdminUser.h

--- AdminUser.h
+++ AdminUser.h
@@ -1,10 +1,7 @@
 class AdminUser : User {
 public:
-    AdminUser() {
-        user_name = "Sougata Ghsoh";
-        email = "ghoshsougataa@gmail.com";
-        password = "Sougata@123";
-    }
+    AdminUser();
+
     void show_all_User();
     void login();
 };
